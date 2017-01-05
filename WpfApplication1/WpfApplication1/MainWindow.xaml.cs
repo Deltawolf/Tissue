47740a756d25af43f32e7d599ee971876407a913
Deltawolf <zachcshore@gmail.com>
2017-01-04 22:17:33 -0600

    Start

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Tissue_Dashboard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// 
+    /// </summary>
+    /// 
+
+
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            TheTracker.TissueTracker.PrepareTheChopper();
+        }
+
+        //private void InitializeComponent()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+
+        internal class TheTracker
+        {
+            public Window main_window { get; set; }
+            public Excel.Application oXL { get; set; }
+            public Excel.Workbook oWB { get; set; }
+            public Excel.Worksheet archivalTracker { get; set; }
+            public Excel.Worksheet freshTracker { get; set; }
+            public Excel.Worksheet sourceTracker { get; set; }
+            public Excel._Worksheet oSheet { get; set; }
+            public Excel.Range oRng { get; set; }
+
+            private static TheTracker Tissuetracker = new TheTracker();
+            private TheTracker() { }
+
+            internal static TheTracker TissueTracker
+            {
+                get
+                {
+                    if (Tissuetracker == null)
+                        Tissuetracker = new TheTracker();
+
+                    return Tissuetracker;
+                }
+            }
+
+            internal void PrepareTheChopper()
+            {
+                oXL = new Excel.Application();
+                string myWorkbook = @"C:\Users\Zach\Desktop\New1.xlsx";
+                oWB = oXL.Workbooks.Open(myWorkbook);
+                oSheet = (Excel._Worksheet)oWB.ActiveSheet;
+
+                oXL.Visible = true;
+                oXL.UserControl = true;
+            }
+        }
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            Window1 tissueRequest = new Window1();
+            tissueRequest.Show();
+            TheTracker.TissueTracker.main_window = this;
+            TheTracker.TissueTracker.main_window.Visibility = Visibility.Collapsed;
+        }
+
+    }
+}
---
+++ WpfApplication1/WpfApplication1/Window1.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
+using Ta = Tissue_Dashboard.MainWindow.TheTracker;
+
+
+namespace Tissue_Dashboard
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        internal Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Ta.TissueTracker.oSheet.Rows[3].Insert();
+            string patientName = patientText.Text;
+            string accessionText1 = accessionText.Text;
+            string requestorText1 = requestorText.Text;
+            string physicianText1 = physicianText.Text ;
+            string enrollmentText1 = enrollmentText.Text;
+            string dateofbirthText1 = dateofbirthText.Text;
+            Ta.TissueTracker.oSheet.Cells[3, 1].value = patientName;
+            Ta.TissueTracker.oSheet.Cells[3, 2].value = accessionText1;
+            Ta.TissueTracker.oSheet.Cells[3, 3].value = requestorText1;
+            Ta.TissueTracker.oSheet.Cells[3, 4].value = physicianText1;
+            Ta.TissueTracker.oSheet.Cells[3, 5].value = enrollmentText1;
+            Ta.TissueTracker.oSheet.Cells[3, 6].value = dateofbirthText1;
+
+            Window parentwin = Window1.GetWindow(this);
+            parentwin.Close();
+            Ta.TissueTracker.main_window.Visibility = Visibility.Visible;
+            
+
+        }
+    }
+}
